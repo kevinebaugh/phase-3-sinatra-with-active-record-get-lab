9a47a6cf30eac380afc026e563eca9e9b747ccbf
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-03-07 14:59:59 -0500

    commit tree 9bde1d3697933d02ecf160fec2ff1e42d3ad0c8a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get "/" do
+    redirect("/bakeries")
+  end
+
+  get "/bakeries" do
+    Bakery.all.to_json
+  end
+
+  get "/bakeries/:id" do
+    Bakery.find(params[:id]).to_json(include: :baked_goods)
+  end
+
+  get "/baked_goods/by_price" do
+    BakedGood.order(price: :desc).to_json
+  end
+
+  get "/baked_goods/most_expensive" do
+    BakedGood.order(price: :desc).first.to_json
+  end
 
 end
